ccbe7ebe4d1c72af9de61965fe93d82044abbec9
Cédric Malet <cedeur@gmail.com>
2021-04-16 12:47:15 +0200

    Update DocString
    Add Doc String and comment in code

--- MassFactoryOrderCalculator/core.py
+++ MassFactoryOrderCalculator/core.py
@@ -13,7 +13,7 @@ if getattr(sys, 'frozen', False):
 else:
     DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
-_JSON_PATH = "{}/ressources/data.json".format(DIR_PATH)
+_JSON_PATH = "{}/ressources/mass_factory_data.json".format(DIR_PATH)
 
 INFO_LIST = ["Unit par crate", "B. Mat", "E. Mat", "H.E. Mat", "R. Mat", "Time (seconds)"]
 
@@ -22,7 +22,7 @@ _RESSOURCES_PATH = "{}/ressources".format(DIR_PATH)
 
 def get_json_data():
     """
-    Extract json datas from data.json file
+    Extract json data from mass_factory_data.json file
     :return:
     """
     with open(_JSON_PATH) as json_access:
@@ -34,6 +34,12 @@ JSON_DATA = get_json_data()
 
 
 def get_info(craft_input):
+    """
+    Get info from specified craft name.
+
+    :param str craft_input: craft name.
+    :return dict: Info for specified craft
+    """
     for category in JSON_DATA:
         for craft in JSON_DATA[category]:
             if craft == craft_input:
@@ -41,81 +47,166 @@ def get_info(craft_input):
 
 
 def convert_resources_info(craft_input, crate_num):
+    """
+    Convert resource info with specified number of crate.
+
+    :param str craft_input: Craft name.
+    :param int crate_num:  Number of crates.
+    :return dict: Converted info.
+    """
+
+    # Get craft info.
     info = get_info(craft_input)
+
+    # Create output dict.
     converted_info_dict = {}
+
+    # Process for each info.
     for inf in info:
+
+        # Check if info are resources.
         if inf in INFO_LIST[1:5]:
+
+            # Set the first step price reduction.
             reduction = 90
+
+            # Set first value to loop on.
             converted_info_dict[inf] = 0
+
+            # Process for each number of crate.
             for i in range(crate_num):
+
+                # Apply reduction on resource price and add it to the total cost.
                 converted_value = converted_info_dict[inf] + int(info[inf] * reduction / 100.0)
+
+                # Check if value not 0, to bypass 0 cost resources.
                 if converted_value != 0:
                     converted_info_dict[inf] = converted_value
+
+                # Check reduction state if more than 50 %, reduce it by 10 %.
                 if not reduction == 50:
                     reduction -= 10
-    return converted_info_dict
-
-
-def convert_info(craft_input, crate_num):
-    info = get_info(craft_input)
-    converted_info_dict = {}
-    for inf in info:
-        if inf in INFO_LIST[1:5]:
-            reduction = 90
-            converted_info_dict[inf] = 0
-            for i in range(crate_num):
-                converted_info_dict[inf] = converted_info_dict[inf] + int(info[inf] * reduction / 100.0)
-                if not reduction == 50:
-                    reduction -= 10
-        elif not isinstance(info[inf], int):
-            converted_info_dict[inf] = info[inf] * crate_num
     return converted_info_dict
 
 
 def convert_time_info(craft_input, crate_num):
+    """
+    Convert time info with specified number of crate.
+
+    :param str craft_input: Craft name.
+    :param int crate_num: Crate number.
+    :return int: Converted time info.
+    """
+
+    # Get craft info.
     info = get_info(craft_input)
-    crate_info = info[INFO_LIST[-1]]
-    converted_info = crate_info * crate_num
+
+    # Get time info.
+    time_info = info[INFO_LIST[-1]]
+
+    # Convert time info
+    converted_info = time_info * crate_num
+
     return converted_info
 
 
 def get_base_info(craft_input, crate_num):
+    """
+    Convert basic info with specified number of crate.
+
+    :param str craft_input: Craft name.
+    :param int crate_num: Crate number.
+    :return int: Converted basic info.
+    """
+
+    # Get craft info.
     info = get_info(craft_input)
+
+    # Get crate info.
     crate_info = info[INFO_LIST[0]]
+
+    # Convert crate info.
     converted_info = crate_info * crate_num
+
     return converted_info
 
 
 def get_craft_category(craft_item):
+    """
+    Retrieve category for a given craft.
+
+    :param str craft_item: Craft name.
+    :return str: Category name.
+    """
     for category in JSON_DATA:
         if craft_item in JSON_DATA[category]:
             return category
 
 
 def get_categories():
+    """
+    Get all categories.
+    :return list: All categories in data.
+    """
     return [category for category in JSON_DATA]
 
 
 def get_crafts(category):
+    """
+    Get all crafts of a given category.
+
+    :param str category: Category name.
+    :return list: All crafts of the category.
+    """
     return [craft for craft in JSON_DATA[category]]
 
 
 def get_category_icon(category):
+    """
+    Get icon associated with the category.
+
+    :param str category: Category name.
+    :return path: Path of the icon.
+    """
     return "{}/Icons_category/{}.png".format(_RESSOURCES_PATH, category)
 
 
 def get_craft_icon(craft):
+    """
+    Get icon associated with the craft.
+
+    :param str craft: Craft name.
+    :return path: Path of the icon.
+    """
+
+    # Get category of the craft.
     category = get_craft_category(craft)
     return "{}/{}/{}.png".format(_RESSOURCES_PATH, category, craft)
 
 
 def get_resource_icon(resource):
+    """
+    Get icon associated with the resource.
+
+    :param str resource: Resource name.
+    :return path: Path of the icon.
+    """
     return "{}/Ressources/{}".format(_RESSOURCES_PATH, resource)
 
 
 def get_crate_icon():
+    """
+    Get the crate icon.
+
+    :return path: Path of the icon.
+    """
     return "{}/Ressources/Crate".format(_RESSOURCES_PATH)
 
 
 def get_time_icon():
+    """
+    Get the time icon.
+
+    :return path: Path of the icon.
+    """
     return "{}/Ressources/Time".format(_RESSOURCES_PATH)
--- MassFactoryOrderCalculator/gui.py
+++ MassFactoryOrderCalculator/gui.py
@@ -21,7 +21,6 @@ class MassFactoryUI(QtWidgets.QWidget):
         self.make_connection()
         self.display_info()
 
-
     def build_ui(self):
         """
         Build the UI using Qt Layouts and Widgets.
@@ -125,9 +124,10 @@ class MassFactoryUI(QtWidgets.QWidget):
         current_list_widget = self.categories_tab_widget.currentWidget()
         current_list_widget.setCurrentItem(current_list_widget.item(0))
 
-
-
     def make_connection(self):
+        """
+        Create connexion between QtSignal and function.
+        """
         self.search_bar_line_edit.textChanged.connect(self.refresh_list_widget)
 
         self.categories_tab_widget.currentChanged.connect(self.on_switch_tab)
@@ -138,7 +138,11 @@ class MassFactoryUI(QtWidgets.QWidget):
         self.crate_number_spinbox.valueChanged.connect(self.display_info)
 
     def create_category_tab_widget(self, category):
+        """
+        Create a tab for each category of craft.
 
+        :param str category: Name of the category.
+        """
         # = List Widget =
         # Create Widget
         self.craft_list_widget = QtWidgets.QListWidget()
@@ -187,11 +191,27 @@ class MassFactoryUI(QtWidgets.QWidget):
         }                          """)
 
     def build_craft_list(self, category, list_widget):
+        """
+        Create all list items in the QlistWidget of the specified category.
+
+        :param str category: Name of the category
+        :param QListWidget list_widget: Parent QListWidget of the QListItemWidgets.
+        """
+
+        # Get all craft of the given category.
         crafts = core.get_crafts(category)
         for craft in crafts:
+
+            # Create an ListWidgetItem parented to the given list_widget.
             item_list_widget = QtWidgets.QListWidgetItem(parent=list_widget)
+
+            # Set ListWidgetItem text.
             item_list_widget.setText(craft)
+
+            # Create QIcon for the given craft.
             craft_icon = QtGui.QIcon(core.get_craft_icon(craft))
+
+            # Set the ListWidgetItem QIcon.
             item_list_widget.setIcon(craft_icon)
 
     def refresh_list_widget(self):
@@ -217,7 +237,6 @@ class MassFactoryUI(QtWidgets.QWidget):
         # Select the first item
         list_widget.setCurrentItem(list_widget.item(0))
 
-
     def rebuild_craft_list(self, filter_craft, category, list_widget):
         """
         Add each craft to de list widget depending if filter is in the craft
@@ -242,35 +261,63 @@ class MassFactoryUI(QtWidgets.QWidget):
                 item_list_widget.setIcon(craft_icon)
 
     def display_info(self):
+        """
+        Call all function that display info in the info layout.
+        And set QSpinbox max range to 5 if current category is Vehicle or Shippable.
+        """
+
+        # Get the current category.
         category = self.categories_tab_widget.tabText(self.categories_tab_widget.currentIndex())
-        if category == "Vehicles" or category == "Shipable":
+
+        # Set the QSpinbox max range to 5 if current category is Vehicle or Shippable.
+        if category == "Vehicles" or category == "Shippable":
             if self.crate_number_spinbox.value() > 5:
                 self.crate_number_spinbox.setValue(5)
+        # Call all function to display info in info layout.
         self.display_base_info()
         self.display_time_info()
         self.display_resources_info()
 
     def display_base_info(self):
+        """
+        Create UI for info layout that display basic info of craft, craft name and crate number.
+        """
+
+        # Clear info layout.
         for i in reversed(range(self.craft_label_layout.count())):
             layout = self.craft_label_layout.takeAt(i).layout()
             for x in reversed(range(layout.count())):
                 layout.takeAt(x).widget().deleteLater()
             layout.deleteLater()
 
+        # Get the current QListWidget.
         selected_craft_list = self.categories_tab_widget.currentWidget()
+
+        # Check if an item is selected.
         if selected_craft_list.selectedItems():
+
+            # Get the current selected item text.
             selected_list_item = selected_craft_list.selectedItems()
             craft = selected_list_item[0].text()
+
+            # Get crate number.
             crate_num = self.crate_number_spinbox.value()
+
+            # Retrieve all calculate information.
             crate_num_info = core.get_base_info(craft, crate_num)
 
+            # Build UI
+            # Craft Name Layout
             craft_name_layout = QtWidgets.QHBoxLayout()
             self.craft_label_layout.addLayout(craft_name_layout)
+
+            # Craft Icon Label
             craft_name_icon_label = QtWidgets.QLabel()
             craft_icon = QtGui.QIcon(core.get_craft_icon(craft))
             craft_pixmap = craft_icon.pixmap(QtCore.QSize(36, 36))
             craft_name_icon_label.setPixmap(craft_pixmap)
 
+            # Craft Name Label
             craft_name_text_label = QtWidgets.QLabel()
             craft_name_text_label.setText(craft)
             craft_name_text_label.setStyleSheet("color: white;")
@@ -278,78 +325,129 @@ class MassFactoryUI(QtWidgets.QWidget):
             font.setPointSize(14)
             craft_name_text_label.setFont(font)
 
+            # Add both label to name layout.
             craft_name_layout.addWidget(craft_name_icon_label)
             craft_name_layout.addWidget(craft_name_text_label)
 
+            # Crate Layout
             crate_num_layout = QtWidgets.QHBoxLayout()
             self.craft_label_layout.addLayout(crate_num_layout)
+
+            # Crate Icon Label.
             crate_name_icon_label = QtWidgets.QLabel()
             crate_icon = QtGui.QIcon(core.get_crate_icon())
             crate_pixmap = crate_icon.pixmap(QtCore.QSize(36, 36))
             crate_name_icon_label.setPixmap(crate_pixmap)
 
+            # Crate Text Label.
             crate_num_text_label = QtWidgets.QLabel()
             crate_num_text_label.setText("Total craft item: {}".format(crate_num_info))
             crate_num_text_label.setStyleSheet("color: white;")
             font.setPointSize(12)
             crate_num_text_label.setFont(font)
 
+            # Add both label to crate layout.
             crate_num_layout.addWidget(crate_name_icon_label)
             crate_num_layout.addWidget(crate_num_text_label)
 
     def display_resources_info(self):
+        """
+        Create UI for info layout that display resources info of craft.
+        """
+
+        # Clear info layout.
         for i in reversed(range(self.resource_label_layout.count())):
             layout = self.resource_label_layout.takeAt(i).layout()
             for x in reversed(range(layout.count())):
                 layout.takeAt(x).widget().deleteLater()
             layout.deleteLater()
 
+        # Get the current QListWidget.
         selected_craft_list = self.categories_tab_widget.currentWidget()
+
+        # Check if an item is selected.
         if selected_craft_list.selectedItems():
+
+            # Get current selected item text.
             selected_list_item = selected_craft_list.selectedItems()
             craft = selected_list_item[0].text()
+
+            # Get crate number.
             crate_num = self.crate_number_spinbox.value()
+
+            # Retrieve calculate information.
             resources_info = core.convert_resources_info(craft, crate_num)
+
+            # Process for each info.
             for resource in resources_info:
+
+                # Bypass resources with 0 cost.
                 if resources_info[resource] != 0:
+
+                    # Build UI
+                    # Resource layout.
                     resource_layout = QtWidgets.QHBoxLayout()
                     self.resource_label_layout.addLayout(resource_layout)
+
+                    # Resource Icon Label
                     resource_icon = QtGui.QIcon(core.get_resource_icon(resource))
                     resource_pixmap = resource_icon.pixmap(QtCore.QSize(36, 36))
-                    resource_text = "{}: {}".format(resource, resources_info[resource])
                     resource_label_widget = QtWidgets.QLabel()
+                    resource_icon_label_widget = QtWidgets.QLabel()
+                    resource_icon_label_widget.setPixmap(resource_pixmap)
+
+                    # Resource Text Label.
+                    resource_text = "{}: {}".format(resource, resources_info[resource])
                     resource_label_widget.setText(resource_text)
                     resource_label_widget.setStyleSheet("color: white;")
                     font = QtGui.QFont()
                     font.setPointSize(12)
                     resource_label_widget.setFont(font)
-                    resource_icon_label_widget = QtWidgets.QLabel()
-                    resource_icon_label_widget.setPixmap(resource_pixmap)
 
+                    # Add both Label to resources layout.
                     resource_layout.addWidget(resource_icon_label_widget)
                     resource_layout.addWidget(resource_label_widget)
 
     def display_time_info(self):
+        """
+        Create UI for info layout that display time info of craft.
+        """
+
+        # Clear info layout.
         for i in reversed(range(self.time_label_layout.count())):
             layout = self.time_label_layout.takeAt(i).layout()
             for x in reversed(range(layout.count())):
                 layout.takeAt(x).widget().deleteLater()
             layout.deleteLater()
 
+        # Get the current QListWidget.
         selected_craft_list = self.categories_tab_widget.currentWidget()
+
+        # Check if an item is selected.
         if selected_craft_list.selectedItems():
+
+            # Get current selected item text.
             selected_list_item = selected_craft_list.selectedItems()
             craft = selected_list_item[0].text()
+
+            # Get crate number.
             crate_num = self.crate_number_spinbox.value()
+
+            # Retrieve calculate information.
             time_info = core.convert_time_info(craft, crate_num)
+
+            # Build UI
+            # Time Layout.
             time_layout = QtWidgets.QHBoxLayout()
             self.time_label_layout.addLayout(time_layout)
 
+            # Time Icon Label
             time_icon_label = QtWidgets.QLabel()
             time_icon = QtGui.QIcon(core.get_time_icon())
             time_pixmap = time_icon.pixmap(QtCore.QSize(36, 36))
             time_icon_label.setPixmap(time_pixmap)
 
+            # Time Text Label
             time_text_label = QtWidgets.QLabel()
             time_text_label.setText("Time in seconds: {}".format(time_info))
             time_text_label.setStyleSheet("color: white;")
@@ -357,23 +455,42 @@ class MassFactoryUI(QtWidgets.QWidget):
             font.setPointSize(12)
             time_text_label.setFont(font)
 
-
+            # Add both label to time layout.
             time_layout.addWidget(time_icon_label)
             time_layout.addWidget(time_text_label)
 
     def add_crate(self):
+        """
+        Set QSpinbox + 1.
+        """
+
+        # Get current QSpinbox value.
         num = self.crate_number_spinbox.value()
-        category = self.categories_tab_widget.tabText(self.categories_tab_widget.currentIndex())
-        if not category == 'Vehicle' or 'Shippable':
-            if num < 9:
-                self.crate_number_spinbox.setValue(num + 1)
+
+        # Check if crate num is not already max value.
+        if num < 9:
+
+            # Set value + 1.
+            self.crate_number_spinbox.setValue(num + 1)
 
     def remove_crate(self):
+        """
+        Set QSpinbox -1
+        """
+
+        # Get current QSpinbox value.
         num = self.crate_number_spinbox.value()
+
+        # Check if crate num is not already min value.
         if num > 1:
+
+            # Set value - 1.
             self.crate_number_spinbox.setValue(num - 1)
 
     def on_switch_tab(self):
+        """
+        Triggered functions when tab is switched. Refresh QListWidget and display info in info layout.
+        """
         self.refresh_list_widget()
         current_list_widget = self.categories_tab_widget.currentWidget()
         current_list_widget.setCurrentItem(current_list_widget.item(0))
